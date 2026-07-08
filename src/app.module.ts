de4ae36ca5d0bec3daa991a041b0258ab45bf4ed
Nikita-Merzlikin <Nikita.Merzlikin@fivesysdev.com>
2026-07-08 17:31:44 +0300

    extra changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MyTestModule } from './my-test/my-test.module';
-import { TestMyModule } from './test-my/test-my.module';
-import { FirstModule } from './first/first.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+
+import { FirstModule } from "./first/first.module";
 
 @Module({
-  imports: [MyTestModule, TestMyModule, FirstModule],
+  imports: [FirstModule],
   controllers: [AppController],
   providers: [AppService],
 })
